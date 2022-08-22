5ab070b7aa14f340cb091f62852726bc01c1ca05
Sirojiddin Khamidov <sirojiddin89@gmail.com>
2022-08-22 16:02:15 +0500

    ++

--- js.js
+++ js.js
@@ -8,10 +8,11 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 const number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector(".number").textContent = number;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-  console.log(guess, typeof guess);
 
   if (!guess) {
     console.log(
@@ -19,7 +20,25 @@ document.querySelector(".check").addEventListener("click", function () {
     );
   } else if (guess == number) {
     document.querySelector(".message").textContent = "You got a number";
-  } else if (guess != number) {
-    document.querySelector(".message").textContent = "Try again";
+    score++;
+    document.querySelector(".score").textContent = score;
+  } else if (guess < number) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too low";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "Lost";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess > number) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too hight";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "Lost";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
